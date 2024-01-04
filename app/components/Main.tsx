05d50be6b22c8a482f1e3c97f55fd22b57262d33
LOLEweb <priveta22@mail.ru>
2024-01-05 00:01:36 +0300

    fix

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -52,7 +52,7 @@ const [data, setData] = useState<Root>()
             <h2 className=' text-3xl md:text-4xl font-bold'>Hello👋</h2>
             <div className=' text-lg md:text-xl'>
               <p>On the Internet, people call me <span className='nick'>dizisxd</span>.</p>
-              <p>I'm a software/web developer from Russia who‘s <Tooltip placement='top' hasArrow
+              <p>I m a software/web developer from Russia who s <Tooltip placement='top' hasArrow
                   label={fractionOfYear.toFixed(6)}><span>17 years old</span></Tooltip> and has been coding for over <span>2 years</span>.
               </p>
               <p>You can find more info in <a className=' font-semibold' href="#">/about</a>.</p>
