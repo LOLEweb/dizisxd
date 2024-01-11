881f73dd688d1ea2b5f880a7f28646ab50e33032
LOLEweb <priveta22@mail.ru>
2024-01-11 22:34:30 +0300

    upd redesign

---
+++ app/russia/page.tsx
@@ -0,0 +1,9 @@
+export default function Russia() {
+    return (
+        <div className="h-screen">
+            <div className=" h-1/3"></div>
+            <div className="bg-blue-600 h-1/3"></div>
+            <div className="bg-red-600 h-1/3"></div>
+        </div>
+    );
+}
